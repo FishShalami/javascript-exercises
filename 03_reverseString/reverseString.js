11c344ee9d78f639bc4bc9ebab276f0758ae0276
FishShalami <fishshalami@gmail.com>
2024-10-28 21:45:04 -0700

    Ex 3 passes all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let finalString = '';
+    if (string === '') {
+        return finalString;
+    } else {
+    finalString = string.split('').reverse().join('')
+    }
+    return finalString;
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = reverseString;
