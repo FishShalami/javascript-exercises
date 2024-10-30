37ece8e1fa3be3844ab1646dd8eb12290c6ceeca
FishShalami <fishshalami@gmail.com>
2024-10-29 22:39:20 -0700

    Passed all tests for ex 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if ((!Number.isInteger(year)) || year <= 0) {
+            return 'ERROR';
+    }
+    console.log('Value supplied is a valid year');
+
+    if (year % 400 === 0) {
+        console.log(`Year is divisible by 400`)
+        return true;
+    } else if (year % 100 == 0) {
+        console.log(`Year is divisible by 100`)
+        return false;
+    } else if (year % 4 === 0) {
+        console.log(`Year is divisible by 4`)
+        return true;
+    } else {
+        console.log(`Not divisible by 4 or 400`)
+        return false;
+    }
 
 };
 
