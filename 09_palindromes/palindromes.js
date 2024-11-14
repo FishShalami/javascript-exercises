e17f566026c81d25f62538f8a4353d8860842be7
FishShalami <fishshalami@gmail.com>
2024-11-14 14:38:42 -0800

    Palindrome exercises completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+        
+    const stringClean = [...string.toLowerCase()].filter(char => /[a-z0-9]/.test(char)); //clean the string input, remove puncutation, letters only, as array
+    return stringClean.join('') === stringClean.slice().reverse().join('');
+    //reverse the string and check conditional
+
+
 
 };
 
