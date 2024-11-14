b5f267b4f1601716956dd16a0c40226a01c01a12
FishShalami <fishshalami@gmail.com>
2024-11-14 13:50:19 -0800

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,33 @@
-const add = function() {
+const add = function(...args) {
+  let sum = 0;
+  args.forEach((i) => sum +=i);
+  return sum;
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(arr) {
+  return arr.reduce((total, item) => total + item, 0);
+};
+
+const multiply = function(arr) {
+ return arr.reduce((product, current) => product * current)
+};
+
+const power = function(a,b) {
+  return a ** b
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1
+  } for (let i = num -1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 	
 };
 
